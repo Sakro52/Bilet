91f9ae74b27083cd88baf870666237a2462c32b6
Sakro52 <mrzambak484@gmail.com>
2025-06-26 19:26:19 +0300

    Add files via upload

---
+++ BILET32/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar[] tovars = new Tovar[3];
+            tovars[0] = new Obuv("Кроссовки", 5000, 42, 2);
+            tovars[1] = new Odejda("Футболка", 1000, "Синий", 5);
+            tovars[2] = new Posuda("Тарелка", 200, 0.5, 10);
+
+            int i;
+            for (i = 0; i < tovars.Length; i++)
+            {
+                Console.WriteLine("Товар " + (i + 1) + ":");
+                tovars[i].Print();
+            }
+        }
+    }
+}
