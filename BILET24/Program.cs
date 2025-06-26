5e15b72df27a6f6a27e20f7c944a1b892958a2e8
Sakro52 <mrzambak484@gmail.com>
2025-06-26 19:13:08 +0300

    Add files via upload

---
+++ BILET24/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите должность (0 - Менеджер, 1 - Разработчик, 2 - Тестировщик): ");
+                int postIndex = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите количество отработанных часов: ");
+                int hours = Convert.ToInt32(Console.ReadLine());
+
+                if (postIndex < 0 || postIndex > 2)
+                {
+                    throw new ArgumentException("Недопустимая должность!");
+                }
+
+                Post worker = (Post)postIndex;
+                Accauntant acc = new Accauntant();
+                bool bonus = acc.AskForBonus(worker, hours);
+                Console.WriteLine("Премия " + (bonus ? "положена" : "не положена"));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено не число!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
