d7e5b44c1a6b416ba9d6e783e67f29c0ac367b07
Sakro52 <mrzambak484@gmail.com>
2025-06-26 18:38:20 +0300

    Add files via upload

---
+++ BILET8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Главная функция, с которой начинается выполнение программы, принимает аргументы командной строки (не используются здесь).
+        {
+            Console.Write("Введите логин: "); // Выводим запрос на ввод логина.
+            string login = Console.ReadLine(); // Считываем введенный логин и сохраняем в переменную login.
+
+            Console.Write("Введите имя: "); // Выводим запрос на ввод имени.
+            string name = Console.ReadLine(); // Считываем введенное имя и сохраняем в переменную name.
+
+            Console.Write("Введите фамилию: "); // Выводим запрос на ввод фамилии.
+            string surname = Console.ReadLine(); // Считываем введенную фамилию и сохраняем в переменную surname.
+
+            Console.Write("Введите возраст: "); // Выводим запрос на ввод возраста.
+            int age = Convert.ToInt32(Console.ReadLine()); // Считываем введенный текст, преобразуем в целое число (int) и сохраняем в переменную age.
+
+            User user = new User(login, name, surname, age); // Создаем объект класса User, передаем логин, имя, фамилию и возраст в конструктор.
+
+            user.ShowInfo(); // Вызываем метод ShowInfo для отображения информации о пользователе.
+        }
+    }
+}
---
+++ BILET8/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class User // Класс User для хранения данных о пользователе.
+    {
+        private string login; // Приватное поле для хранения логина пользователя.
+        private string name; // Приватное поле для хранения имени пользователя.
+        private string surname; // Приватное поле для хранения фамилии пользователя.
+        private int age; // Приватное поле для хранения возраста пользователя.
+        private readonly DateTime dateFilled; // Приватное поле для даты заполнения, доступное только для чтения (устанавливается при создании).
+
+        public User(string login, string name, string surname, int age) // Конструктор класса, принимает логин, имя, фамилию и возраст.
+        {
+            this.login = login; // Присваиваем переданный логин полю login.
+            this.name = name; // Присваиваем переданное имя полю name.
+            this.surname = surname; // Присваиваем переданную фамилию полю surname.
+            this.age = age; // Присваиваем переданный возраст полю age.
+            this.dateFilled = DateTime.Now; // Устанавливаем текущую дату и время в поле dateFilled (не изменяется после создания).
+        }
+
+        public void ShowInfo() // Метод для отображения информации о пользователе.
+        {
+            Console.WriteLine($"Логин: {login}"); // Выводим логин пользователя.
+            Console.WriteLine($"Имя: {name}"); // Выводим имя пользователя.
+            Console.WriteLine($"Фамилия: {surname}"); // Выводим фамилию пользователя.
+            Console.WriteLine($"Возраст: {age}"); // Выводим возраст пользователя.
+            Console.WriteLine($"Дата заполнения: {dateFilled:dd.MM.yyyy}"); // Выводим дату заполнения в формате день.месяц.год.
+        }
+    }
+}
