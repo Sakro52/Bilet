42903c4fa3f7422384d66115eac67c6dadc0543a
Sakro52 <mrzambak484@gmail.com>
2025-06-26 19:10:58 +0300

    Add files via upload

---
+++ BILET23/Printer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    enum Colors
+    {
+        Красный,
+        Синий,
+        Зелёный
+    }
+    static class Printer
+    {
+        public static void Print(string stroka, int color)
+        {
+            if (color < 0 || color > 2)
+            {
+                throw new ArgumentException("Недопустимый номер цвета!");
+            }
+
+            if (color == (int)Colors.Красный)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (color == (int)Colors.Синий)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (color == (int)Colors.Зелёный)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ BILET23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите строку: ");
+                string text = Console.ReadLine();
+                Console.Write("Выберите цвет (0 - Красный, 1 - Синий, 2 - Зелёный): ");
+                int color = Convert.ToInt32(Console.ReadLine());
+
+                Printer.Print(text, color);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: цвет должен быть числом!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
