298d7c5dd4b3ec4d0ce0ff5622abca51e2033b9a
Sakro52 <mrzambak484@gmail.com>
2025-06-26 19:04:00 +0300

    Add files via upload

---
+++ BILET17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ISwitchable> devices = new List<ISwitchable>();
+            devices.Add(new TVSet());
+            devices.Add(new PersonalComputer());
+
+            int i;
+            for (i = 0; i < devices.Count; i++)
+            {
+                devices[i].TurnOn();
+                devices[i].TurnOff();
+                Console.WriteLine();
+            }
+        }
+    }
+}
