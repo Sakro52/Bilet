812b2af55d96a715330b32ca22e3b4a0735f5410
Sakro52 <mrzambak484@gmail.com>
2025-06-26 18:37:26 +0300

    Add files via upload

---
+++ BILET9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassRoom classRoom = new ClassRoom(// создание класса ClassRoom с учениками
+                new ExcellentPupil("Иванов"),// создание класса ExcellentPupil
+                new GoodPupil("Петров"),
+                new BadPupil("Сидоров"),
+                new GoodPupil("Козлов")
+            );
+            classRoom.ShowPupils();// вывод информации об учениках
+        }
+    }
+}
