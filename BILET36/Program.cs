6180a9c44c05ebeb480ee5946f79f42d7f555aca
Sakro52 <mrzambak484@gmail.com>
2025-06-26 19:32:25 +0300

    Add files via upload

---
+++ BILET36/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Human[] people = new Human[]
+                {
+                    new Russian("Ivan"),
+                    new Ukrainian("Oleh"),
+                    new American("John")
+                };
+
+                for (int i = 0; i < people.Length; i++)
+                {
+                    people[i].SayGreeting();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
