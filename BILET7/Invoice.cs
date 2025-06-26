709b38929db19e0ad004b0f9e02b00437faf383d
Sakro52 <mrzambak484@gmail.com>
2025-06-26 18:27:37 +0300

    Add files via upload

---
+++ BILET7/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Invoice // Класс Invoice для управления данными о счете-фактуре.
+    {
+        private readonly int account; // Приватное поле для номера счета, доступное только для чтения (не изменяется после создания).
+        private readonly string customer; // Приватное поле для имени покупателя, доступное только для чтения.
+        private readonly string provider; // Приватное поле для имени поставщика, доступное только для чтения.
+        private string article; // Приватное поле для наименования товара, может изменяться.
+        private int quantity; // Приватное поле для количества товара, может изменяться.
+
+        public Invoice(int account, string customer, string provider) // Конструктор класса, принимает номер счета, имя покупателя и поставщика.
+        {
+            this.account = account; // Присваиваем переданный номер счета полю account.
+            this.customer = customer; // Присваиваем переданное имя покупателя полю customer.
+            this.provider = provider; // Присваиваем переданное имя поставщика полю provider.
+        }
+
+        public void SetOrder(string article, int quantity) // Метод для установки данных о заказе (наименование товара и количество).
+        {
+            this.article = article; // Присваиваем переданное наименование товара полю article.
+            this.quantity = quantity; // Присваиваем переданное количество полю quantity.
+        }
+
+        public (double withVAT, double withoutVAT) CalculateCost(double pricePerUnit) // Метод для расчета стоимости с НДС и без НДС, возвращает кортеж.
+        {
+            double withoutVAT = pricePerUnit * quantity; // Рассчитываем общую стоимость без НДС, умножая цену за единицу на количество.
+            double withVAT = withoutVAT * 1.20; // Рассчитываем стоимость с НДС, умножая стоимость без НДС на 1.20 (20% НДС).
+            return (withVAT, withoutVAT); // Возвращаем кортеж с стоимостью с НДС и без НДС.
+        }
+    }
+}
---
+++ BILET7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Главная функция, с которой начинается выполнение программы, принимает аргументы командной строки (не используются здесь).
+        {
+            Invoice invoice = new Invoice(123456, "Иван Иванов", "ООО Поставщик"); // Создаем объект класса Invoice с номером 123456, покупателем "Иван Иванов" и поставщиком "ООО Поставщик".
+
+            invoice.SetOrder("Ноутбук", 5); // Устанавливаем заказ: товар "Ноутбук" в количестве 5 единиц.
+
+            Console.Write("Введите цену за единицу: "); // Выводим запрос на ввод цены за одну единицу товара.
+            double price = Convert.ToDouble(Console.ReadLine()); // Считываем введенный текст, преобразуем в число с плавающей точкой (double) и сохраняем в переменную price.
+
+            var (withVAT, withoutVAT) = invoice.CalculateCost(price); // Вызываем метод CalculateCost, передаем цену, получаем кортеж с стоимостью с НДС (withVAT) и без НДС (withoutVAT).
+
+            Console.WriteLine($"Стоимость без НДС: {withoutVAT:F2}"); // Выводим стоимость без НДС с двумя знаками после запятой (F2).
+            Console.WriteLine($"Стоимость с НДС: {withVAT:F2}"); // Выводим стоимость с НДС с двумя знаками после запятой (F2).
+        }
+    }
+}
