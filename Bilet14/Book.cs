5751a872b57432e72dd5bcbcd11b166feeb9b0e9
Sakro52 <mrzambak484@gmail.com>
2025-06-27 10:05:24 +0300

    Add files via upload

---
+++ Bilet14/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        struct Book
+        {
+            private string title;// Приватное поле для хранения значение титле
+            private string author;
+            private int year;
+            private int pages;
+        public Book(string title, string author, int year, int pages)
+            {
+                this.title = title;
+                this.author = author;
+                this.year = year;
+                this.pages = pages;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine("Название: " + title);
+                Console.WriteLine("Автор: " + author);
+                Console.WriteLine("Год издания: " + year);
+                Console.WriteLine("Количество страниц: " + pages);
+                Console.WriteLine();
+            }
+        }
+    }
+
+
